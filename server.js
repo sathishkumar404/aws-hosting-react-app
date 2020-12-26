0bac41d17e93997e4484be577bffb74c84e9c5c9
Sathish Kumar <sathish@sigaramtech.com>
2020-12-26 08:00:58 +0530

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path  = require('path');
+const PORT =8000; 
+
+
+app.use(express.static(path.join(__dirname,"client","build")))
+
+
+app.listen(PORT,()=>{
+    console.log('port connected',PORT)
+})
